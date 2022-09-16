a5e8283cffa01d4f3550e5b94b04f5192e44e87c
Kev9794 <k.natanael88@gmail.com>
2022-09-16 16:32:33 +0700

    Android Part B

---
+++ app/src/main/java/jti/kevin_natanael_wijaya_partb/MainActivity.java
@@ -0,0 +1,63 @@
+package jti.kevin_natanael_wijaya_partb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    private Button mButtonZero;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = (TextView)findViewById(R.id.show_count);
+        mButtonZero = (Button)findViewById(R.id.button_zero);
+
+        setColor(findViewById(R.id.button_count), mCount % 2 == 0 ? Color.YELLOW : Color.CYAN);
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+    public void countUp(View view) {
+        ++mCount;
+        if(mShowCount!=null){
+            mButtonZero.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            if(mCount%2 == 0){
+                view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            }
+            else{
+                view.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+            mShowCount.setText(Integer.toString(mCount));
+
+            setColor(view, mCount % 2 == 0 ? Color.YELLOW : Color.CYAN);
+            setColor(findViewById(R.id.button_zero), Color.GREEN);
+        }
+
+    }
+    public void zeroCount(View view)
+    {
+        mCount = 0;
+        mShowCount.setText("0");
+
+        setColor(view, Color.parseColor("#ffaaaaaa"));
+        setColor(findViewById(R.id.button_count), Color.YELLOW);
+    }
+    private void setColor(View view, int color)
+    {
+        view.setBackgroundColor(color);
+    }
+}
